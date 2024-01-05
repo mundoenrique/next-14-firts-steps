f656b9f7c8aaa3a51945dab252784f4bda1d972c
Enrique Peñaloza <epenaloza@novopayment.com>
2024-01-05 16:29:20 -0400

    Componentes del cliente

--- components/index.ts
+++ components/index.ts
@@ -1,1 +1,3 @@
+// client components
+export { ActiveLink } from './active-link/ActiveLink';
 export * from './navbar/Navbar';
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import { HomeIcon } from '@primer/octicons-react';
 import Link from 'next/link';
+import { ActiveLink } from '..';
 
 const navItems = [
   { path: '/about', text: 'About' },
@@ -17,9 +18,7 @@ export const Navbar = () => {
       </Link>
       <div className="flex flex-1"></div>
       {navItems.map((navItem) => (
-        <Link key={navItem.path} className="mr-2" href={navItem.path}>
-          {navItem.text}
-        </Link>
+        <ActiveLink key={navItem.path} {...navItem} />
       ))}
     </nav>
   );
